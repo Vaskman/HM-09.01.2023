9e7f8934c32d4bcebf9381bffd43ccc3b51a9089
Vaskman <z.vaskman@gmail.com>
2023-01-09 22:24:17 +0300

    task 7- ok

--- src/Main.java
+++ src/Main.java
@@ -88,10 +88,15 @@ public class Main {
         int one=1;
         int two=2;
         int three=3;
+        if (one >=three ||two>=three){
+            System.out.println("То Меньше "+three+"");}
+        else {
+            System.out.println("То ,больше "+three+"");}
+        }
 
         }
 
-    }
+
 
 
 
