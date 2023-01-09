e05b849d194b74389e209f3fda5aedc95dae8674
Vaskman <z.vaskman@gmail.com>
2023-01-09 19:58:22 +0300

    task 4- ok

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,58 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Home Work 09.01.2023");
-        //task1
-        System.out.println("ЗАДАЧА №1");
-        int age = 17;
-        System.out.println("Если возраст человека равен "+age+ " то");
-        if (age >=18){
-            System.out.println("он совершеннолетний");}
-        else {
-        System.out.println("он не достиг совершеннолетия, нужно немного подождать");}
-        //task2
-        System.out.println("ЗАДАЧА №2");
-        int temperature = 4;
-        System.out.println("На улице "+temperature+ " градусов");
-        if (temperature <5){
-            System.out.println("На улице холодно, нужно надеть шапку");}
-        else {
-            System.out.println("Сегодня тепло, можно идти без шапки");}
-        //task3
-        System.out.println("ЗАДАЧА №3");
-        int speed =55;
-        System.out.println("«Если скорость " +speed+ " то ");
-        if (speed>60){
-            System.out.println("придется заплатить штраф");}
-        else {
-            System.out.println("можно ездить спокойно");}
-        //task4
-        System.out.println("ЗАДАЧА №4");
-
-
+        task1();
+        task2();
+        task3();}
+        //"Home Work 09.01.2023");
+        public static void task1 () {
+            System.out.println("Задача 1");
+            int age = 17;
+            System.out.println("Если возраст человека равен " + age + " то");
+            if (age >= 18) {
+                System.out.println("он совершеннолетний");
+            } else {
+                System.out.println("он не достиг совершеннолетия, нужно немного подождать");}
+            //task2
+            System.out.println("ЗАДАЧА №2");
+            int temperature = 4;
+            System.out.println("На улице " + temperature + " градусов");
+            if (temperature < 5) {
+                System.out.println("На улице холодно, нужно надеть шапку");
+            } else {
+                System.out.println("Сегодня тепло, можно идти без шапки");}
+            //task 3
+            System.out.println("ЗАДАЧА №3");
+            int speed = 55;
+            System.out.println("«Если скорость " + speed + " то ");
+            if (speed > 60) {
+                System.out.println("придется заплатить штраф");
+            } else {
+                System.out.println("можно ездить спокойно");}
+        }
+        public static void task2 () {
+            System.out.println("Задача 4");
+            int age =25;
+            System.out.println("Если возраст человека равен " +age+ " то ему нужно ходить");
+            if (age >=2 && age <=6 ){
+                System.out.println(" в детский сад");}
+            if (age >=7 && age <=18){
+                System.out.println(" в школу");}
+            if (age >=18 && age <=24){
+                System.out.println(" в университет");}
+            if (age >24){
+                System.out.println(" на работу");}
+    }
+    public static void task3 (){
+        System.out.println("Задача 5");
 
     }
 }
+
+
+
+
+
+
+
+
+
