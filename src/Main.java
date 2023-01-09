5d3961dae00fae4551f4191ea471579b6e8439c3
Vaskman <z.vaskman@gmail.com>
2023-01-09 15:05:44 +0300

    task 1- ok

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Home Work 09.01.2023");
-    }
-    //task1
+        //task1
+        System.out.println("ЗАДАЧА №1");
+        int age = 17;
+        System.out.println("Если возраст человека равен "+age+ " то");
+        if (age >=18){
+            System.out.println("он совершеннолетний");}
+        else {
+        System.out.println("он не достиг совершеннолетия, нужно немного подождать");}
+        //task2
+        System.out.println("ЗАДАЧА №2");
 
+
+    }
 }
