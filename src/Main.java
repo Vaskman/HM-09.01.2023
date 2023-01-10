ec418a7277f41532a77ff69baf8b4f379455887e
Vaskman <z.vaskman@gmail.com>
2023-01-10 16:34:47 +0300

    HM- ok

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
     //"Home Work 09.01.2023");
     public static void task1() {
         System.out.println("Задача 1");
-        int age = 17;
+        int age = 21;
         System.out.println("Если возраст человека равен " + age + " то");
         if (age >= 18) {
             System.out.println("он совершеннолетний");
@@ -44,13 +44,13 @@ public class Main {
         if (age >= 2 && age <= 6) {
             System.out.println(" в детский сад");
         }
-        if (age >= 7 && age <= 18) {
+        else if (age >= 7 && age <= 18) {
             System.out.println(" в школу");
         }
-        if (age >= 18 && age <= 24) {
+        else if (age >= 18 && age <= 24) {
             System.out.println(" в университет");
         }
-        if (age > 24) {
+        else if (age > 24) {
             System.out.println(" на работу");
         }
     }
@@ -62,17 +62,17 @@ public class Main {
         if (age < 5) {
             System.out.println(" нельзя кататься на аттракционе");
         }
-        if (age >= 5 && age < 14) {
+        else if (age >= 5 && age < 14) {
             System.out.println("можно кататься на аттракционе в сопровождении");
         }
-        if (age >= 14) {
+        else if (age >= 14) {
             System.out.println("можно кататься на аттракционе без сопровождения взрослого");
         }
     }
 
     public static void task4() {
         System.out.println("Задача 6");
-        int capacity = 103;
+        int capacity = 101;
         System.out.println("вы " + capacity + " пассажир осталось мест");
         if (capacity <= 60)
             System.out.println("есть сидячие места");
